5f5a625d52fe844ced357285ca35a619e955c9fa
Alexandra Fomina <aleksa.fom@gmail.com>
2017-07-31 13:51:34 +0300

    some Bloxorz

--- streams/src/main/scala/streams/StringParserTerrain.scala
+++ streams/src/main/scala/streams/StringParserTerrain.scala
@@ -65,7 +65,10 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val vcId = levelVector.indexWhere(_.contains(c))
+    Pos(vcId, levelVector(vcId).indexOf(c))
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
