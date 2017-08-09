5c59e8fb09ba54ddb73fa171eead472cdf99f8fa
alexandra <aleksa.fom@gmail.com>
2017-08-09 14:34:52 +0300

    polynomial solver

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,16 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+    Var(b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Var(
+      if (delta() < 0) Set()
+      else if (delta() == 0) Set(-b() / 2 * a())
+      else Set((-b() + Math.sqrt(delta())) / (2 * a()), (-b() - Math.sqrt(delta())) / (2 * a()))
+    )
   }
 }
