e3479ce43438f7bdfb0b02ac6d4d66728be864b4
alexandra <aleksa.fom@gmail.com>
2017-09-20 10:40:55 +0300

    classified columns

--- timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -71,7 +71,7 @@ object TimeUsage {
     * @param line Raw fields
     */
   def row(line: List[String]): Row =
-    ???
+    Row.fromSeq(line)
 
   /** @return The initial data frame columns partitioned in three groups: primary needs (sleeping, eating, etc.),
     *         work and other (leisure activities)
@@ -89,7 +89,11 @@ object TimeUsage {
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
   def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
+    def createColumns(filter: List[String]): List[Column] = columnNames.filter(s => filter.exists(s.startsWith)).map(col)
+
+    (createColumns(List("t01", "t03", "t11", "t1801", "t1803")),
+      createColumns(List("t05", "t1805")),
+      createColumns(List("t02", "t04", "t06", "t07", "t08", "t09", "t10", "t12", "t13", "t14", "t15", "t16", "t18")))
   }
 
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
